910fb3c8207889f50e573be9c534889c91b9c4ee
Tony MacDonell <tony@teknision.com>
2018-10-25 15:41:18 -0400

    Base functionality implemented on top of Convo list implementation.

--- index.js
+++ index.js
@@ -1,68 +1,102 @@
-const {ConvoApp, Convo} = require('@tmtek/convo');
+const {ConvoApp, Convo, Say} = require('@tmtek/convo');
 
 class BulletinBoard extends ConvoApp {
 
-  onRegisterIntents() {
-    this.registerIntent('welcome', (convo, params, option, debug) =>
-      Convo.close(convo.promise(() => {
-          if (this.hasBulletins(convo)) {
-            return this.readBulletins(convo);
-          } else {
-            return convo.speak('Welcome to BulletinBoard. To add something to your board, just say: add, and then the message');
-          }
-      }),debug)
-    );
+	onRegisterIntents() {
+		this.registerListIntents();
+		this.registerIntent('welcome', (convo, params, option, debug) =>
+		  Convo.ask(convo.promise(() => {
+		      if (this.hasBulletins(convo)) {
+		        return this.readBulletins(convo);
+		      } else {
+		        return convo.speak('Welcome to BulletinBoard. To add something to your board, just say: add, and then the message');
+		      }
+		  }),debug)
+		);
+		this.registerIntent('add_bulletin', (convo, {bulletin}, option, debug) =>
+		  Convo.ask(this.addBulletin(convo,bulletin), debug)
+		);
+		this.registerIntent('read_bulletins', (convo, params, option, debug) =>
+		  Convo.ask(this.readBulletins(convo, ConvoApp.ensureNumber(params.count)),debug)
+		);
+		this.registerIntent('read_bulletins_all', (convo, params, option, debug) =>
+		  Convo.close(this.readBulletins(convo, -1),debug)
+		);
+		this.registerIntent('clear_bulletins', (convo, params, option, debug) =>
+		  Convo.ask(this.clearBulletins(convo), debug)
+		);
+		this.registerIntent('bulletin_delete', (convo, params, option, debug) =>
+		  Convo.ask(this.deleteBulletin(convo), debug)
+		);
+	}
 
-    this.registerIntent('add_bulletin', (convo, {bulletin}, option, debug) =>
-      Convo.close(this.addBulletin(convo,bulletin),debug)
-    );
+	onRespondForList({ convo, type, page, paging, list }) {
+		if (type === 'bulletin') {
+			return convo.speak(Say.listPageResponse(page, paging, list, item => Say.ensureSentence(item.text), "\n\n"));
+		}
+		return convo.speak(`Where's my list?:${JSON.stringify(convo.getContext('list'))}`);
+	}
 
-    this.registerIntent('read_bulletins', (convo, params, option, debug) =>
-      Convo.close(this.readBulletins(convo),debug)
-    );
+	onRespondForListSelection({ convo, type, item }) {
+		if (type === 'bulletin' && item) {
+			return convo.speak(`${item.text}`);
+		}
+		return convo.speak('Nothing is selected');
+	}
 
-    this.registerIntent('clear_bulletins', (convo, params, option, debug) =>
-      Convo.close(this.clearBulletins(convo), debug)
-    );
-  }
+	onQueryListForSelection(type, item, query) {
+		if (type === 'bulletin' && item) {
+			return new RegExp(query.toLowerCase()).test(item.text.toLowerCase());
+		}
+		return false;
+	}
 
   hasBulletins(convo) {
     return convo.isInStorage("bulletins", list => list.length > 0);
   }
 
-  readBulletins(convo) {
-    if (this.hasBulletins(convo)) {
-      let bulletins = convo.getFromStorage("bulletins");
-      convo.speak("Here's what's on the board:")
-      bulletins.forEach(bulletin => {
-				convo.speak(this.ensureSentence(bulletin.text));
-			});
-    } else {
-      convo.speak("There's nothing on your board. To add something to your board, just say: add, and then the message")
-    }
-    return convo;
-  }
+	readBulletins(convo, count = 5) {
+		let correctedCount = count === 0 ? 5 : count;
+		if (this.hasBulletins(convo)) {
+		  let bulletins = convo.getFromStorage("bulletins");
+		  return convo
+				.setList('bulletin', bulletins, {start:0, count:correctedCount})
+				.forListPage(this.onRespondForList);
+		}
+		return convo.speak("There's nothing on your board. To add something to your board, just say: add, and then the message");
+	}
 
   addBulletin(convo, message) {
-    var bulletins = [];
-    if (this.hasBulletins(convo)) {
-        bulletins = convo.getFromStorage("bulletins");
-    }
-    bulletins.push({text:message, added: new Date().valueOf()});
-    convo.setToStorage("bulletins", bulletins);
-    return convo.speak(`Adding:${message}`);
+	    let bulletins = this.hasBulletins(convo) ? convo.getFromStorage("bulletins") : [];
+	    bulletins.unshift({text:message, added: new Date().valueOf()});
+	    return convo
+				.setToStorage("bulletins", bulletins)
+				.updateList(bulletins)
+				.speak(`Adding:${message}`);
   }
 
   clearBulletins(convo) {
-    convo.setToStorage("bulletins", []);
-    return convo.speak(`Cleared all bulletins.`);
+	    return convo
+				.setToStorage("bulletins", [])
+				.clearList()
+				.speak(`Cleared all bulletins.`);
   }
 
-	ensureSentence(sentence) {
-		if (!/\.$/.test(sentence)) {
-			return `${sentence}.`;
-		}
-		return sentence;
+	deleteBulletin(convo) {
+		return convo.promise(() => {
+			let selection = convo.getListSelection();
+			if (selection && selection.type === 'bulletin') {
+				let bulletins = convo.getFromStorage("bulletins");
+				let bulletin = bulletins[selection.index];
+				bulletins.splice(selection.index,1);
+				return convo
+					.setToStorage("bulletins", bulletins)
+					.updateList(bulletins)
+					.speak(`Deleted:${bulletin.text}`);
+			}
+			return convo.speak('There was no bulletin to delete.');
+		})
+
 	}
 }
 
--- test.js
+++ test.js
@@ -2,12 +2,15 @@ const {BulletinBoard} = require('./index');
 const {Convo, ConvoStorage} = require('@tmtek/convo');
 
 
-new ConvoStorage("storage.json")
-	.load(storageConvo => {
+let cs = new ConvoStorage("storage.json")
+	cs.load(storageConvo => {
 		new BulletinBoard()
 		  .intent(storageConvo, 'welcome', null, null, {log:true})
+			.then(({app, convo}) => app.intent(new Convo(convo), 'list_find', {query:'poop'}, null, {log:true}))
+			//.then(({app, convo}) => app.intent(new Convo(convo), 'list_next', null, null, {log:true}))
+			//.then(({app, convo}) => app.intent(new Convo(convo), 'list_select', null, null, {log:true}))
 			//.intent(storageConvo, 'read_bulletins', null, null, {log:true})
-		  //.then(({app, convo}) => app.intent(new Convo(convo), 'add_bulletin', {text:"Phin is not going to the bday party"}, null, {log:false}))
+		  //.then(({app, convo}) => app.intent(new Convo(convo), 'add_bulletin', {text:"Give Seb a bath"}, null, {log:true}))
 		  //.then(({app, convo}) => app.intent(new Convo(convo), 'add_bulletin', {text:"Mel goes out with Jarra Saturday night."}, null, {log:false}))
 		  //.then(({app, convo}) => app.intent(new Convo(convo), 'read_bulletins', null, null, {log:true}))
 		  //.then(({app, convo}) => app.intent(new Convo(convo), 'clear_bulletins', null, null, {log:true}))
